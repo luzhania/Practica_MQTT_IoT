ca19333fbad6646e95bbb68ec96f8bb280572a99
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-10-14 17:03:08 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,92 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Configuración WiFi
+const char* ssid = "HUAWEI-2.4G-M6xZ";
+const char* password = "HT7KU2Xv";
+
+// Configuración MQTT
+const char* mqtt_server = "broker.hivemq.com"; // Broker MQTT público
+const int mqtt_port = 1883; // Puerto del broker
+const char* mqtt_topic = "titos/place/sonido"; // Tema MQTT
+
+// Pines del sensor KY-037
+const int soundSensorPin = 32; // Asegúrate de usar un pin analógico
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Función para conectarse al WiFi
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Conectado a WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// Función para reconectar MQTT en caso de desconexión
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a client ID based on the ESP32's unique chip ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Subscribe to the topic to receive LED control messages
+      client.subscribe("titos/place/test");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(soundSensorPin, INPUT);
+  
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setKeepAlive(180);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Leer el valor del sensor KY-037
+  int sensorValue = analogRead(soundSensorPin);
+  Serial.print("Nivel de sonido: ");
+  Serial.println(sensorValue);
+
+  // Convertir valor a String
+  String payload = String(sensorValue);
+
+  // Publicar los datos del sensor en el tema MQTT
+  if (client.publish(mqtt_topic, payload.c_str())) {
+    Serial.println("Datos enviados exitosamente");
+  } else {
+    Serial.println("Error al enviar los datos");
+  }
+
+  delay(5000); // Esperar 5 segundos antes de la siguiente lectura
+}
