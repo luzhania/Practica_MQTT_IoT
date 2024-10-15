32ad434333b4c868ab2c0f0a777b72f0dc91a2a8
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-10-15 11:46:57 -0400

    Implementing observer design pattern for relay and mqtt sensor publisher

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <vector>
+
 #define RELAY_PIN 26
 #define SOUND_SENSOR_PIN 35
 
 using namespace std;
-class WiFiConnection
-{
+
+class WiFiConnection {
 private:
-  const char *SSID;
-  const char *PASSWORD;
+  const char* SSID;
+  const char* PASSWORD;
 
 public:
-  WiFiConnection(const char *SSID, const char *PASSWORD)
+  WiFiConnection(const char* SSID, const char* PASSWORD)
       : SSID(SSID), PASSWORD(PASSWORD) {}
 
-  void connect()
-  {
+  void connect() {
     WiFi.begin(SSID, PASSWORD);
-    while (WiFi.status() != WL_CONNECTED)
-    {
+    while (WiFi.status() != WL_CONNECTED) {
       delay(1000);
       Serial.println("Connecting to WiFi...");
     }
@@ -29,128 +28,162 @@ public:
 
 class Observer {
 public:
-    virtual void update(unsigned int data) = 0;
+  virtual void update(const String& message) = 0;
 };
 
-class MQTTClient : public Observer {
+class MQTTSensorPublisher : public Observer {
 private:
-    PubSubClient client;
-    const char* SERVER;
-    const unsigned int PORT;
-    const char* TOPIC;
+  PubSubClient client;
+  const char* SERVER;
+  const unsigned int PORT;
+  const char* publishTopic;
 
 public:
-    MQTTClient(Client& espClient, const char* SERVER, unsigned int PORT, const char* TOPIC) 
-        : client(espClient), SERVER(SERVER), PORT(PORT), TOPIC(TOPIC) {}
+  MQTTSensorPublisher(Client& espClient, const char* SERVER, unsigned int PORT, const char* publishTopic)
+      : client(espClient), SERVER(SERVER), PORT(PORT), publishTopic(publishTopic) {}
 
-    void connect() {
-        client.setServer(SERVER, PORT);
-        reconnect();
-    }
+  void connect() {
+    client.setServer(SERVER, PORT);
+    reconnect();
+  }
 
-    void reconnect() {
-        while (!client.connected()) {
-            String clientId = "ESP32Client-" + String(random(0xffff), HEX);
-            if (client.connect(clientId.c_str())) {
-                Serial.println("Connected to MQTT broker");
-            } else {
-                Serial.print("Error MQTT connection, rc=");
-                Serial.println(client.state());
-                delay(5000);
-            }
-        }
+  void reconnect() {
+    while (!client.connected()) {
+      String clientId = "ESP32Client-" + String(random(0xffff), HEX);
+      if (client.connect(clientId.c_str())) {
+        Serial.println("Connected to MQTT broker");
+      } else {
+        Serial.print("Error MQTT connection, rc=");
+        Serial.println(client.state());
+        delay(5000);
+      }
     }
+  }
 
-    void publish(const String& message) {
-        if (client.publish(TOPIC, message.c_str())) {
-            Serial.println("Data sent successfully");
-        } else {
-            Serial.println("Error sending data");
-        }
+  void publish(const String& message) {
+    if (client.publish(publishTopic, message.c_str())) {
+      Serial.println("Data sent successfully");
+    } else {
+      Serial.println("Error sending data");
     }
+  }
 
-    void update(unsigned int data) override {
-        publish(String(data));
+  void loop() {
+    if (!client.connected()) {
+      reconnect();
     }
+    client.loop();
+  }
 
-    void loop() {
-        if (!client.connected()) {
-            reconnect();
-        }
-        client.loop();
-    }
+  PubSubClient& getClient() {
+    return client;
+  }
+
+  void update(const String& message) override {
+    publish(message);
+  }
 };
 
-// class RelayControl : public Observer {
-// private:
-//     int relayPin;
+class RelayObserver : public Observer {
+private:
+  int relayPin;
 
-// public:
-//     RelayControl(int relayPin) : relayPin(relayPin) {
-//         pinMode(relayPin, OUTPUT);
-//     }
+public:
+  RelayObserver(int relayPin) : relayPin(relayPin) {
+    pinMode(relayPin, OUTPUT);
+  }
 
-//     void update(unsigned int data) override {
-//         if (data > 1000) {
-//             digitalWrite(relayPin, HIGH);
-//         } else {
-//             digitalWrite(relayPin, LOW);
-//         }
-//     }
-// };
+  void update(const String& message) override {
+    if (message == "ON") {
+      digitalWrite(relayPin, HIGH);
+      Serial.println("Relay ON");
+    } else if (message == "OFF") {
+      digitalWrite(relayPin, LOW);
+      Serial.println("Relay OFF");
+    }
+  }
+};
 
 class Subject {
 private:
-    vector<Observer*> observers;
+  vector<Observer*> observers;
 
 public:
-    void attach(Observer* observer) {
-        observers.push_back(observer);
-    }
+  void attach(Observer* observer) {
+    observers.push_back(observer);
+  }
 
-    void detach(Observer* observer) {
-        observers.erase(remove(observers.begin(), observers.end(), observer), observers.end());
-    }
+  void detach(Observer* observer) {
+    observers.erase(remove(observers.begin(), observers.end(), observer), observers.end());
+  }
 
-    void notifyObservers(unsigned int data) {
-        for (Observer* observer : observers) {
-            observer->update(data);
-        }
+  void notifyObservers(const String& message) {
+    for (Observer* observer : observers) {
+      observer->update(message);
     }
+  }
 };
 
 class SoundSensor : public Subject {
 private:
-    unsigned int pin;
+  unsigned int pin;
 
 public:
-    SoundSensor(unsigned int pin) : pin(pin) {}
+  SoundSensor(unsigned int pin) : pin(pin) {}
 
-    void checkSoundLevel() {
-        unsigned int sensorValue = analogRead(pin);
-        Serial.print("Sound level: ");
-        Serial.println(sensorValue);
-        notifyObservers(sensorValue);
+  void checkSoundLevel() {
+    unsigned int sensorValue = analogRead(pin);
+    Serial.print("Sound level: ");
+    Serial.println(sensorValue);
+    notifyObservers(String(sensorValue));
+  }
+};
+
+class MQTTActuatorController : public Subject {
+private:
+  PubSubClient& client;
+
+public:
+  MQTTActuatorController(PubSubClient& client) : client(client) {}
+
+  void callback(char* topic, byte* payload, unsigned int length) {
+    String message;
+    for (int i = 0; i < length; i++) {
+      message += (char)payload[i];
     }
+    Serial.print("Message received: ");
+    Serial.println(message);
+
+    notifyObservers(message);
+  }
+
+  void setCallback() {
+    client.setCallback([this](char* topic, byte* payload, unsigned int length) {
+      callback(topic, payload, length);
+    });
+  }
 };
 
 WiFiClient espClient;
 WiFiConnection wifi("HUAWEI-2.4G-M6xZ", "HT7KU2Xv");
-MQTTClient mqtt(espClient, "broker.hivemq.com", 1883, "titos/place/sound");
+MQTTSensorPublisher mqttSensorPublisher(espClient, "broker.hivemq.com", 1883, "titos/place/sound");
+MQTTActuatorController mqttActuatorController(mqttSensorPublisher.getClient());
+RelayObserver relayObserver(RELAY_PIN);
 SoundSensor soundSensor(SOUND_SENSOR_PIN);
-// RelayControl relay(RELAY_PIN);
 
 void setup() {
-    Serial.begin(115200);
-    wifi.connect();
-    mqtt.connect();
-    
-    soundSensor.attach(&mqtt);  // MQTT observará los cambios del sensor de sonido
-    // soundSensor.attach(&relay); // El relé observará los cambios del sensor de sonido
+  Serial.begin(115200);
+  wifi.connect();
+  mqttSensorPublisher.connect();
+
+  mqttActuatorController.attach(&relayObserver);
+  mqttActuatorController.setCallback();
+  
+  soundSensor.attach(&mqttSensorPublisher);
 }
 
 void loop() {
-    mqtt.loop();
-    soundSensor.checkSoundLevel();
-    delay(1000);
+  mqttSensorPublisher.loop();
+  soundSensor.checkSoundLevel();
+  delay(1000);
 }
